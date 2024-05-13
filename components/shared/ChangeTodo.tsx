9473cde6508af4caf81501d4d55331e7d89423fb
danielaruiz1 <daniela.ruizgar1@gmail.com>
2024-05-13 13:11:06 -0700

    todo page

---
+++ components/shared/ChangeTodo.tsx
@@ -0,0 +1,23 @@
+import Form from "../ui/Form";
+import Button from "../ui/Button";
+import { AiOutlineCheckCircle } from "react-icons/ai";
+import Input from "../ui/input";
+import { changeStatus } from "@/app/actions/todoActions";
+import { todoType } from "@/types/todoType";
+
+const ChangeTodo = ({ todo }: { todo: todoType }) => {
+  return (
+    <Form action={changeStatus}>
+      <input
+        name="inputId"
+        value={todo.id}
+        className="border-gray-700 border"
+        type="hidden"
+      />
+
+      <Button actionButton type="submit" text={<AiOutlineCheckCircle />} />
+    </Form>
+  );
+};
+
+export default ChangeTodo;
